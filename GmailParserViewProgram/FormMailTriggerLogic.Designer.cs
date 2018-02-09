32a5b2c05c85a156d5afabaa26eae4d0b4ca5a87
OlegChernyshev <chernyshev360@gmail.com>
2018-02-09 12:08:36 +0300

    0.3

--- GmailParserViewProgram/FormMailTriggerLogic.Designer.cs
+++ GmailParserViewProgram/FormMailTriggerLogic.Designer.cs
@@ -50,8 +50,16 @@
             this.Tick = new System.Windows.Forms.Timer(this.components);
             this.cb_autorun = new System.Windows.Forms.CheckBox();
             this.l_version = new System.Windows.Forms.Label();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.l_counterMessage = new System.Windows.Forms.Label();
+            this.l_fileCounter = new System.Windows.Forms.Label();
+            this.l_counter = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.l_status = new System.Windows.Forms.Label();
+            this.l_count = new System.Windows.Forms.Label();
             this.listViewMenuStrip.SuspendLayout();
             this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label3
@@ -67,7 +75,6 @@
             // 
             // tb_local
             // 
-
             this.tb_local.Font = new System.Drawing.Font("Arial Narrow", 12F);
             this.tb_local.Location = new System.Drawing.Point(71, 32);
             this.tb_local.Name = "tb_local";
@@ -174,7 +181,7 @@
             this.exitToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(533, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(532, 24);
             this.menuStrip1.TabIndex = 19;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -215,7 +222,7 @@
             // Tick
             // 
             this.Tick.Enabled = true;
-            this.Tick.Interval = 400;
+            this.Tick.Tick += new System.EventHandler(this.Tick_Tick);
             // 
             // cb_autorun
             // 
@@ -239,13 +246,92 @@
             this.l_version.TabIndex = 21;
             this.l_version.Text = "v 0.1";
             // 
+            // btn_start
+            // 
+            this.btn_start.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_start.Location = new System.Drawing.Point(271, 18);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(219, 43);
+            this.btn_start.TabIndex = 22;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // l_counterMessage
+            // 
+            this.l_counterMessage.AutoSize = true;
+            this.l_counterMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.l_counterMessage.Location = new System.Drawing.Point(28, 52);
+            this.l_counterMessage.Name = "l_counterMessage";
+            this.l_counterMessage.Size = new System.Drawing.Size(45, 16);
+            this.l_counterMessage.TabIndex = 23;
+            this.l_counterMessage.Text = "label2";
+            // 
+            // l_fileCounter
+            // 
+            this.l_fileCounter.AutoSize = true;
+            this.l_fileCounter.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.l_fileCounter.Location = new System.Drawing.Point(28, 79);
+            this.l_fileCounter.Name = "l_fileCounter";
+            this.l_fileCounter.Size = new System.Drawing.Size(45, 16);
+            this.l_fileCounter.TabIndex = 24;
+            this.l_fileCounter.Text = "label2";
+            // 
+            // l_counter
+            // 
+            this.l_counter.AutoSize = true;
+            this.l_counter.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.l_counter.Location = new System.Drawing.Point(28, 25);
+            this.l_counter.Name = "l_counter";
+            this.l_counter.Size = new System.Drawing.Size(45, 16);
+            this.l_counter.TabIndex = 25;
+            this.l_counter.Text = "label2";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.l_count);
+            this.groupBox1.Controls.Add(this.l_status);
+            this.groupBox1.Controls.Add(this.btn_start);
+            this.groupBox1.Controls.Add(this.l_counter);
+            this.groupBox1.Controls.Add(this.l_counterMessage);
+            this.groupBox1.Controls.Add(this.l_fileCounter);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.groupBox1.Location = new System.Drawing.Point(16, 426);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(502, 108);
+            this.groupBox1.TabIndex = 26;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Data";
+            // 
+            // l_status
+            // 
+            this.l_status.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.l_status.Location = new System.Drawing.Point(271, 64);
+            this.l_status.Name = "l_status";
+            this.l_status.Size = new System.Drawing.Size(219, 38);
+            this.l_status.TabIndex = 26;
+            this.l_status.Text = "label2";
+            this.l_status.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // l_count
+            // 
+            this.l_count.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.l_count.Location = new System.Drawing.Point(163, 64);
+            this.l_count.Name = "l_count";
+            this.l_count.Size = new System.Drawing.Size(119, 38);
+            this.l_count.TabIndex = 27;
+            this.l_count.Text = "label2";
+            this.l_count.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // FormMailTriggerLogic
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.SystemColors.WindowFrame;
-            this.ClientSize = new System.Drawing.Size(533, 432);
+            this.ClientSize = new System.Drawing.Size(532, 546);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.l_version);
             this.Controls.Add(this.cb_autorun);
             this.Controls.Add(this.menuStrip1);
@@ -260,13 +346,14 @@
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "FormMailTriggerLogic";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "FormMailTriggerLogic";
             this.Text = "Settings";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormMailTriggerLogic_FormClosed);
             this.Load += new System.EventHandler(this.FormMailTriggerLogic_Load);
             this.listViewMenuStrip.ResumeLayout(false);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -295,5 +382,12 @@
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.CheckBox cb_autorun;
         private System.Windows.Forms.Label l_version;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Label l_counterMessage;
+        private System.Windows.Forms.Label l_fileCounter;
+        private System.Windows.Forms.Label l_counter;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label l_status;
+        private System.Windows.Forms.Label l_count;
     }
 }
--- GmailParserViewProgram/Model/GMessage.cs
+++ GmailParserViewProgram/Model/GMessage.cs
@@ -11,10 +11,18 @@ using Google.Apis.Gmail.v1;
 using Google.Apis.Gmail.v1.Data;
 using Google.Apis.Services;
 
+using System.Text.RegularExpressions;
+
 namespace GmailParserViewProgram.Model
 {
     public class GMessage
     {
+        //[25240] 		[218]	"160fa0707e77a766"	string
+        //[17538]	"15f9506f0de9e628"	string
+
+        static public string lastMessageId = "";
+
+
         public string messageId;
         public string raw;
         public MessagePartHeader headers;
@@ -32,8 +40,15 @@ namespace GmailParserViewProgram.Model
 
         // "16146f5b41668414"
 
+        static public int messageFind = 0;
+        static public int counter = 0;
+        static public int fileFind = 0;
+        static public int countMessage = 0;
+
         public byte[] GetFile (List<string> messageId, string path)
         {
+            counter = messageId.Capacity;
+            fileFind = 0;
             byte[] data = null;
             foreach (string id in messageId)
             {
@@ -55,6 +70,7 @@ namespace GmailParserViewProgram.Model
                         data = Convert.FromBase64String(attachData);
                         //string strin = Encoding.ASCII.GetString(data);
                         //stream.Write(data, 0, data.Length);
+                        ++fileFind; counter--;
                         File.WriteAllBytes(Path.Combine(path, part.Filename), data);
                     }
                 }
@@ -135,16 +151,63 @@ namespace GmailParserViewProgram.Model
         /// <returns>Возвращает список id сообщений с которыми совподает теги поиска</returns>
         public List<string> Find(GRule gRule, List<string> ids)
         {
+            counter = 0;
+            messageFind = 0;
+            List<string> listId = new List<string>();
+            foreach (string id in ids)
+            {
+                ++counter;
+                var messageData = service.Users.Messages.Get("me", id);
+                messageData.Format = UsersResource.MessagesResource.GetRequest.FormatEnum.Metadata;
+                IList<MessagePartHeader> headers = messageData.Execute().Payload.Headers;
+
+                string subject = String.Empty;
+
+                foreach (var item in headers)
+                {
+                    if (item.Name == "Subject")
+                    {
+                        subject = item.Value;
+                        break;
+                    }
+                }
+
+                bool find = true;
+                string[] strs = Regex.Split(gRule.tag , "\\s");
+
+                foreach (string item in strs)
+                    if (!Regex.IsMatch(subject, item))
+                    {
+                        find = false;
+                        break;
+                    }
+                    else
+                    {
+                        find = true;
+                    }
+
+
+                if (find) { listId.Add(id); ++messageFind; }
+            }
+            return listId;
+        }
+
+        public List<string> Find(List<GRule> gRules, List<string> ids)
+        {
             List<string> listId = new List<string>();
             foreach (string id in ids)
             {
                 var messageData = service.Users.Messages.Get("me", id);
                 messageData.Format = UsersResource.MessagesResource.GetRequest.FormatEnum.Metadata;
                 IList<MessagePartHeader> headers = messageData.Execute().Payload.Headers;
+
                 foreach (var val in headers)
-                    foreach (GRule item in GRule.GetGrules())
+                    foreach (GRule item in gRules)
                         if (val.Value == item.tag)
                             listId.Add(id);
+                         
+
+
             }
             return listId;
         }
@@ -157,15 +220,39 @@ namespace GmailParserViewProgram.Model
             });
         }
 
-
         public List<string> GetMessages()
         {
-            IList<Message> messages = service.Users.Messages.List(email).Execute().Messages;
+            List<Message> messages = new List<Message>();
+            UsersResource.MessagesResource.ListRequest request = service.Users.Messages.List(email);
+            //var s = service.Users.Threads.List(email);
             List<string> ids = new List<string>();
-            foreach (Message item in messages)
+
+            do
             {
-                ids.Add(item.Id);
-            }
+                try
+                {
+                    ListMessagesResponse response = request.Execute();
+                    messages.AddRange(response.Messages);
+                    //countMessage = messages.Capacity;
+                    request.PageToken = response.NextPageToken;
+
+                    foreach (Message item in messages)
+                    {
+                        if (item.Id != lastMessageId)
+                        {
+                            ids.Add(item.Id);
+                            countMessage = ids.Capacity;
+                        }
+                        else return ids;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("An error occurred: " + e.Message);
+                }
+            } while (!String.IsNullOrEmpty(request.PageToken));
+
+            
             return ids;
         }
 
